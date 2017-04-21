a177728791e34be68a1608400999702fa84bad7f
SupeDeDupe <sethompson@upei.ca>
2017-04-21 17:42:21 -0400

    Video Additions

--- P3/ui.py
+++ P3/ui.py
@@ -1,3 +1,7 @@
+
+import kivy
+from kivy.app import App
+from kivy.properties     import *
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
@@ -7,8 +11,71 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.scatter import Scatter
 from kivy.uix.button import Button
+from kivy.animation     import * 
+from kivy.uix.videoplayer import Video
+from kivy.uix.button     import Button
+from functools import partial
+
+# Import kivy osc library
+from kivy.lib.osc         import oscAPI 
+# Import clock (required by osc listener)
+from kivy.clock           import Clock
 
 class UI(FloatLayout):
+    vid_state = 0
+    vidLength = 141.0 # in seconds
+    start = 0
+    stop = 0
+    vid_pos = (720,200)
+
+    def playVideo(self, start, stop, value):
+        print "THIS BUTTON WORKS"
+        self.start = start
+        self.stop = stop
+        try:
+            if self.vid_state == 0:
+                self.renderer.opacity = 0
+
+                #play video
+                self.vid1_scatter.pos = self.vid_pos
+                self.video.opacity = 1
+                self.video.play = True
+                self.video.seek(float(self.start/self.vidLength))
+                self.vid_state = 1
+
+            else:
+                #stop video
+                self.video.opacity = 0
+                self.video.play = False
+                self.vid_state = 0
+                self.renderer.opacity = 1
+        except:
+            if self.vid_state == 0:
+
+                #play video
+                self.vid1_scatter.pos = self.vid_pos
+                self.video.opacity = 1
+                self.video.play = True
+                self.video.seek(float(self.start/self.vidLength))
+                self.vid_state = 1
+
+            else:
+                #stop video
+                self.video.opacity = 0
+                self.video.play = False
+                self.vid_state = 0
+
+    def cb_seek(self, instance, value):
+        if value > self.stop:
+            #stop video
+            self.video.opacity = 0
+            self.video.play = False
+            self.vid_state = 0
+            try:
+                self.renderer.opacity = 1
+            except:
+                pass
+
     def __init__(self, **kwargs):
         super(UI, self).__init__(**kwargs)
         self.screenType = kwargs['screenType']
@@ -23,9 +90,6 @@ class UI(FloatLayout):
         self.controller = kwargs['controller']
         self.renderer = kwargs['renderer']
         #self.renderer.pos = (1500, 1500)
-        self.add_widget(self.renderer)
-
-        #self.renderer.opacity = 0
 
         # Defining colors
         # use a (r, g, b, a) tuple (a = alpha channel = tranparency)
@@ -34,7 +98,7 @@ class UI(FloatLayout):
 
         # instantiate the labels
         self.label1 =  Label(text="3-D Brain Model", font_size=250, color=self.black, font_name='fonts/Living Hell.ttf')
-        self.label1Wid = Widget(pos = (0,0))
+        self.label1Wid = Widget(pos = (0,0), size=(self.label1.size))
         if self.screenType == "horizontal":
             self.label1.pos=(self.width+1000, self.height+850)
         else:
@@ -43,7 +107,7 @@ class UI(FloatLayout):
         self.label1Wid.add_widget(self.label1)
 
         self.label2 =  Label(text="Rotate X", font_size=50, bold = True, color=self.grey, pos=(self.width+1320, 0))
-        self.label2Wid = Widget(pos = (0, 0))
+        self.label2Wid = Widget(pos = (0, 0), size=(self.label2.size))
         if self.screenType == "horizontal":
             self.label2.opacity = 1
         else:
@@ -55,7 +119,7 @@ class UI(FloatLayout):
             self.label3.opacity = 1
         else:
             self.label3.opacity = 0
-        self.label3Wid = Widget(pos = (0, 0))
+        self.label3Wid = Widget(pos = (0, 0), size=(self.label3.size))
         self.label3Wid.add_widget(self.label3)
 
         self.label4 =  Label(text="Zoom", font_size=50, bold = True, color=self.grey, pos=(self.width+1940, 0))
@@ -63,8 +127,103 @@ class UI(FloatLayout):
             self.label4.opacity = 1
         else:
             self.label4.opacity = 0
-        self.label4Wid = Widget(pos = (0, 0))
+        self.label4Wid = Widget(pos = (0, 0), size=(self.label4.size))
         self.label4Wid.add_widget(self.label4)
+
+        # Basic facts about the brain
+        self.widget1 = Widget(pos = (0, 0), size=(300, 300))
+        button1 = Button(background_normal='Tile1/Vid-2.png', size=self.widget1.size, pos = self.widget1.pos)
+        button1.bind(on_press=partial(self.playVideo, 0, 12))
+        self.widget1.add_widget(button1)
+
+        # weight of brain
+        self.widget2 = Widget(pos = (0, 300), size=(300, 300))
+        button2 = Button(background_normal='Tile1/Vid-3.png', size=self.widget2.size, pos = self.widget2.pos)
+        button2.bind(on_press=partial(self.playVideo, 13, 21))
+        self.widget2.add_widget(button2)
+        
+        # Brain is protected by bones, meninges, and spinal fluid
+        self.widget3 = Widget(pos = (0, 0), size=(300, 300))
+        button3 = Button(background_normal='Tile1/Vid-4.png', size=self.widget3.size, pos = (0, 300))
+        button3.bind(on_press=partial(self.playVideo, 22, 33))
+        self.widget3.add_widget(button3)
+        
+        # brain has 3 parts: fore brain, mid brain, and hind brain
+        self.widget4 = Widget(pos = (0, 0), size=(300, 300))
+        button4 = Button(background_normal='Tile1/Vid-5.png', size=self.widget4.size, pos = (0, 300))
+        button4.bind(on_press=partial(self.playVideo, 34, 41))
+        self.widget4.add_widget(button4)
+
+        ########################### 6 6 6
+        # Fore brain
+        self.widget5 = Widget(pos = (0, 0), size=(300, 300))
+        button5 = Button(background_normal='Tile1/Vid-6.png', size=self.widget5.size, pos = (0, 300))
+        button5.bind(on_press=partial(self.playVideo, 41, 61))
+        self.widget5.add_widget(button5)
+        
+        # The Cerebrum...
+        self.widget6 = Widget(pos = (0, 0), size=(300, 300))
+        button6 = Button(background_normal='Tile1/Vid-7.png', size=self.widget6.size, pos = (0, 300))
+        button6.bind(on_press=partial(self.playVideo, 61, 78))
+        self.widget6.add_widget(button6)
+        
+        ########################### 8 8 8
+        # Mid brain
+        self.widget7 = Widget(pos = (0, 0), size=(300, 300))
+        button7 = Button(background_normal='Tile1/Vid-8.png', size=self.widget7.size, pos = (0, 300))
+        button7.bind(on_press=partial(self.playVideo, 78, 89))
+        self.widget7.add_widget(button7)
+
+        ########################### 9 9 9
+        # Hind brain
+        self.widget8 = Widget(pos = (0, 0), size=(300, 300))
+        button8 = Button(background_normal='Tile1/Vid-9.png', size=self.widget8.size, pos = (0, 300))
+        button8.bind(on_press=partial(self.playVideo, 89, 89))
+        self.widget8.add_widget(button8)
+        
+        # Cerebellum... balance, movements, coordination
+        self.widget9 = Widget(pos = (0, 0), size=(300, 300))
+        button9 = Button(background_normal='Tile1/Vid-10.png', size=self.widget9.size, pos = (0, 300))
+        button9.bind(on_press=partial(self.playVideo, 96, 106))
+        self.widget9.add_widget(button9)
+        
+        # Pons and Medulla / midbrain .. breathing, heart rate, blood pressure, swallowing, digestion, and blinking
+        self.widget10 = Widget(pos = (0, 0), size=(300, 300))
+        button10 = Button(background_normal='Tile1/Vid-11.png', size=self.widget10.size, pos = (0, 300))
+        button10.bind(on_press=partial(self.playVideo, 106, 126))
+        self.widget10.add_widget(button10)
+
+        ########################### 12 12 12 
+        # Different centres of our brain -- video summary
+        widget11 = Widget(pos = (0, 0), size=(300, 300))
+        button11 = Button(ackground_normal='Tile1/Vid-12.png', size=widget11.size, pos = (0, 300))
+        button11.bind(on_press=partial(self.playVideo, 126, 126))
+        widget11.add_widget(button11)
+
+        self.video = Video(size = (800,800), pos=(0,0), volume=1.0, 
+                         options={'eos': 'loop'}, allow_stretch = True,
+            keep_ratio = True, opacity =0)
+        self.video.source = 'Tile1/Tile1-Vid.mp4' 
+        self.video.play = False
+        self.video.bind(position=self.cb_seek)
+
+        #Creates a widget to add the vid_scatter to
+        self.vid1_wid = Widget()
+        # Creates a scatter widget
+        self.vid1_scatter = Scatter(size=(800, 800), pos=self.vid_pos)
+        # Adds image to the scatter, and the scatter to the widget
+        self.vid1_scatter.add_widget(self.video)
+        self.vid1_wid.add_widget(self.vid1_scatter)
+
+        # Button widgets
+        self.add_widget(self.widget1)
+        self.add_widget(self.widget2)
+
+        if self.screenType == "vertical":
+            self.add_widget(self.vid1_wid)
+            self.add_widget(self.renderer)
+        else:
+            self.video.volume = 0
 
         # add label widgets to the main layout 
         self.add_widget(self.label1Wid)
@@ -253,11 +412,14 @@ class UI(FloatLayout):
 
     def on_touch_down(self, touch):
         if (self._within(touch)):
+            print "first elif"
             super(UI, self).on_touch_down(touch)
         else:
+            print "else statement"
             self._touch = touch
             touch.grab(self)
             self._touches.append(touch)
+            super(UI, self).on_touch_down(touch)
         
     def on_touch_up(self, touch):
         if (self._within(touch)):
